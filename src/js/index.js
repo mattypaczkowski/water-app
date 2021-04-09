5854a5ee2bcb14e7771319b5f9ca1c0637c46a64
Mateusz Paczkowski <mattypaczkowski@gmail.com>
2021-04-09 12:21:15 +0100

    work with local storage

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,56 @@
 import '../scss/main.scss';
 
 // uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
+import {registerSW} from './pwa.js';
+registerSW();
+
+
 
 /* place your code below */
 
 console.log('HELLO 🚀')
+
+// add queryselector to button add
+const addButton = document.querySelector('.waterApp__button--add');
+// add queryselector to button remove
+const removeButton = document.querySelector('.waterApp__button--remove');
+// add queryselector to count numbers
+const glassCounter = document.querySelector('.waterApp__numbers');
+//current data 
+const key = new Date().toLocaleString().slice(0, 10);
+
+let currentCounter = 0;
+
+const localStorageValue = localStorage.getItem(key);
+
+if (localStorageValue) {
+ currentCounter = localStorageValue;
+} else {
+  localStorage.setItem(key, 0);
+}
+
+glassCounter.innerHTML = currentCounter;
+
+addButton.addEventListener('click', () => {
+  if (currentCounter < 99) {
+  currentCounter++;
+  glassCounter.innerHTML = currentCounter;
+  localStorage.setItem(key, currentCounter);
+  }
+})
+
+removeButton.addEventListener('click', () => {
+  if (currentCounter > 0) {
+  currentCounter--;
+  glassCounter.innerHTML = currentCounter;
+  localStorage.setItem(key, currentCounter);
+  }
+ })
+
+
+ const historyButton = document.querySelector('.waterApp__history--js')
+
+ historyButton.addEventListener('click', () => {
+  const table = document.querySelector('.table')
+  table.classList.toggle('table--js')
+})
--- webpack.common.js
+++ webpack.common.js
@@ -88,5 +88,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/history.html",
+      inject: true,
+      chunks: ["index", "another", "history"],
+      filename: "history.html",
+    }),
   ],
 };
